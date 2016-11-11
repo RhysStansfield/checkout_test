b0c938def106ffdd5be0d180bb992029893795a1
Rhys Stansfield <r.stansfield@voc-8.com>
2016-11-11 00:48:31 +0000

    Add total, subtotal and discount amounts to line item

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -1,8 +1,17 @@
 class LineItem
-  attr_accessor :product, :quantity
+  attr_accessor :product, :quantity, :discount_amount
 
   def initialize(product)
     self.product = product
     self.quantity = 1
+    self.discount_amount = 0.00
+  end
+
+  def subtotal
+    product.price * quantity
+  end
+
+  def total
+    subtotal - discount_amount
   end
 end
--- test/line_item_test.rb
+++ test/line_item_test.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(__FILE__, '..', 'test_helper'))
 
 class TestLineItem < Minitest::Test
   def setup
-    @product = Product.new
+    @product = Product.new('001', 'Lavender', 5.00)
     @line_item = LineItem.new(@product)
   end
 
@@ -11,6 +11,22 @@ class TestLineItem < Minitest::Test
   end
 
   def test_has_quantity
-    assert_same 1, @line_item.quantity
+    assert_equal 1, @line_item.quantity
+  end
+
+  def test_has_subtotal
+    assert_equal 5.00, @line_item.subtotal
+  end
+
+  def test_has_discount_amount
+    assert_equal 0.00, @line_item.discount_amount
+  end
+
+  def test_has_total_affected_by_discount_amount
+    assert_equal 5.00, @line_item.total
+
+    @line_item.discount_amount = 1.00
+
+    assert_equal 4.00, @line_item.total
   end
 end
