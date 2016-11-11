d6f2d1d2d11de732a580909bc72dc94340e36fa0
Rhys Stansfield <r.stansfield@voc-8.com>
2016-11-11 01:30:35 +0000

    Add checkout

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,37 @@
 class Checkout
+  attr_accessor :order, :promotions
+
   def initialize(promotions)
+    self.order = Order.new
+    self.promotions = promotions
+  end
+
+  def scan(product)
+    reset_discount_amounts
+
+    order.add(product)
+
+    apply_promotions
+
+    self
   end
 
   def total
+    "£#{sprintf('%05.2f', order.total)}"
+  end
+
+  private
+
+  def apply_promotions
+    eligible_promotions.each { |promotion| promotion.apply_to(order) }
+  end
+
+  def eligible_promotions
+    promotions.select { |promotion| promotion.eligible?(order) }
+  end
+
+  def reset_discount_amounts
+    order.discount_amount = 0.00
+    order.line_items.each { |line_item| line_item.discount_amount = 0.0 }
   end
 end
--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -1,10 +1,41 @@
 require File.expand_path(File.join(__FILE__, '..', 'test_helper'))
 
 class TestCheckout < Minitest::Test
-  attr_accessor :checkout
-
   def setup
-    promotions = []
-    self.checkout = Checkout.new(promotions)
+    @product = Product.new('001', 'Lavender', 5.00)
+    @promotions = []
+    @checkout = Checkout.new(@promotions)
+  end
+
+  def test_sets_passed_in_promotion_rules
+    assert_same @promotions, @checkout.promotions
+  end
+
+  def test_creates_order
+    assert_kind_of Order, @checkout.order
+  end
+
+  def test_can_scan_product
+    assert_equal 0, @checkout.order.line_items.count
+
+    @checkout.scan(@product)
+
+    assert_equal 1, @checkout.order.line_items.count
+  end
+
+  def test_applies_promotions
+    rule = -> (order) { order.total >= 15.00 }
+    action = -> (order) { order.discount_amount = 5.00 }
+    @checkout.promotions << Promotion.new(rule, action)
+
+    3.times { @checkout.scan(@product) }
+
+    assert_equal 10.0, @checkout.order.total
+  end
+
+  def test_total
+    3.times { @checkout.scan(@product) }
+
+    assert_equal '£15.00', @checkout.total
   end
 end
